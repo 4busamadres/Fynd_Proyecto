1f1331eeb4883387b651003f69a4915c19cf76ac
eJwtzLFuwjAUheF38ZyhMSRgz5WKhKjaigEmy7GvwY2T617HUIp491oo29GRvv/OtDGQ0h57GJlk7aLhYARwpxsu1iv7AksreNPVy7pdaG6EcXVnHKtYJLx4CzTT+1xS05w6nVG9Hhw/bI+iWZN7nzbQfuDuJ33n3ddqeDvW9Jf9Z0k9iZpuEYrrQBNQeZPB56Ht4EcZcxe8UT3cKgJtJdKpjIhVTkASBu1DdUXqXcBrwfAbPUFSemJyzCE8Hv+f8lCu <novoaparra97@gmail.com>
2023-12-08 03:04:38 -0300

    Avance mio

--- app/src/main/java/com/example/fynd_proyecto/Menu_parada.java
+++ app/src/main/java/com/example/fynd_proyecto/Menu_parada.java
@@ -2,17 +2,75 @@ package com.example.fynd_proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Menu_parada extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private ListView lv_paradas;
+    private ParadaAdapter paradaAdapter;
+    private List<Map<String, Object>> todasLasParadas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_parada);
+
+        db= FirebaseFirestore.getInstance();
+        lv_paradas=findViewById(R.id.lv_paradas);
+
+        todasLasParadas = new ArrayList<>();
+        paradaAdapter = new ParadaAdapter(this, todasLasParadas);
+        lv_paradas.setAdapter(paradaAdapter);
+
+
+        registrarParada();
+        EditText txtBuscar = findViewById(R.id.txt_parada_Destino);
+        Button btnBuscar = findViewById(R.id.btn_parada_buscar);
+
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buscarParadas(txtBuscar.getText().toString());
+            }
+        });
     }
+    public void registrarParada()
+    {
+        Map<String, Object> map = new HashMap<>();
+        map.put("nomParada", "Mendoza / Rengo");
+
+        // Añadir la parada al ArrayList y notificar al adaptador
+        todasLasParadas.add(map);
+        paradaAdapter.notifyDataSetChanged();
+
+    }
+    private void buscarParadas(String query) {
+        List<Map<String, Object>> paradasFiltradas = new ArrayList<>();
+        for (Map<String, Object> parada : todasLasParadas) {
+            String nomParada = parada.get("nomParada").toString().toLowerCase();
+            if (nomParada.contains(query.toLowerCase())) {
+                paradasFiltradas.add(parada);
+            }
+        }
+        paradaAdapter.clear();
+        paradaAdapter.addAll(paradasFiltradas);
+        paradaAdapter.notifyDataSetChanged();
+    }
+
     public void volver_menu(View v){
         Intent volver_menu=new Intent(Menu_parada.this, Menu.class);
         startActivity(volver_menu);
--- app/src/main/java/com/example/fynd_proyecto/Menu_soporte.java
+++ app/src/main/java/com/example/fynd_proyecto/Menu_soporte.java
@@ -1,14 +1,68 @@
 package com.example.fynd_proyecto;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Menu_soporte extends AppCompatActivity {
+    EditText txt_nombre, txt_correo, txt_problema;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_soporte);
+        txt_nombre=findViewById(R.id.txt_soporte_nombre);
+        txt_correo=findViewById(R.id.txt_soporte_correo);
+        txt_problema=findViewById(R.id.txt_soporte_problema);
+        db=FirebaseFirestore.getInstance();
+    }
+    public void mostrarAlerta(String titulo, String mensaje)
+    {
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+        alerta.setTitle(titulo);
+        alerta.setMessage(mensaje);
+        alerta.setPositiveButton("Aceptar", null);
+        AlertDialog dialogo=alerta.create();
+        dialogo.show();
+    }
+    public void gestionarProblema(View v)
+    {
+        String nombre=txt_nombre.getText().toString();
+        String correo= txt_correo.getText().toString();
+        String problema=txt_problema.getText().toString();
+        Map<String,Object> map =new HashMap<>();
+        map.put("Nombre", nombre);
+        map.put("Correo",correo);
+        map.put("Problema", problema);
+        db.collection("Soporte"). add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                mostrarAlerta("Soporte Tecnico","Requerimiento registrado correctamente");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mostrarAlerta("Soporte Tecnico", "Algo sucedio, requerimiento no ingresado"+ e.getMessage());
+            }
+        });
+
+    }
+    public void volver_menu(View v){
+        Intent volver_menu=new Intent(Menu_soporte.this, Menu.class);
+        startActivity(volver_menu);
     }
 }
